cc16bd08aba5671b44931c0758bcbda128b1c0e4
David Shamoev <shamoevdavid04@gmail.com>
2024-06-30 18:06:37 +0500

    firs commit

---
+++ store/products/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import ProductCategory, Product, Basket
+
+# Register your models here.
+
+admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',) #фильтрация
+    ordering = ('name',)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
+
---
+++ store/users/forms.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from .models import User
+from django import forms
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter username'}))
+
+    password = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter password'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter first name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter second name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter username'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter your password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class' : 'form-control py-4', 'placeholder' : 'Enter your password (again)'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'image', 'username', 'email']
